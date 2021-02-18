f1413dd32472b4108c811c31d192848a0ca775ec
Gastón <gaston@pragmasoft.com>
2021-02-18 16:17:10 -0300

    Iniciación del proyecto y manipulación de datos.

---
+++ src/app/pages/registro/registro.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.usuario = new UsuarioModel();
+    this.usuario.email = 'prueba@email.com'
+  }
+
+  onSubmit( form: NgForm ) {
+    
+    if(form.invalid){
+      return;
+    }
+
+    console.log('Formulario enviado');
+    console.log(this.usuario);
+    console.log(form);
+  }
+
+}
