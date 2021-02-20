6d185d0704830b8f50014b7a3d19190dfe66d511
Gastón <gaston@pragmasoft.com>
2021-02-20 09:54:21 -0300

    Controles de campos agregados en el formulario de Login.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from '../../models/usuario.model';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  usuario: UsuarioModel;
+
   constructor() { }
 
   ngOnInit() {
+    this.usuario = new UsuarioModel();
+  }
+
+  login(form: NgForm) {
+    if(form.invalid) {
+      console.info('El formaulario es inválido.');
+    }
+
+    console.log(form);
   }
 
 }
--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -15,7 +15,6 @@ export class RegistroComponent implements OnInit {
 
   ngOnInit() {
     this.usuario = new UsuarioModel();
-    this.usuario.email = 'prueba@email.com'
   }
 
   onSubmit( form: NgForm ) {
